a57012d3c423f6f77a0f4ab8945eda1e87c4028e
abeshaw97 <shawabra@msu.edu>
2022-03-06 16:34:46 -0500

    added start button/timer along with css

--- Assets/JS/quiz.js
+++ Assets/JS/quiz.js
@@ -1,1 +1,33 @@
-window.alert("This is an alert! JavaScript is running!");
+function startTimer(){
+    var counter = 50;
+    setInterval(function() {
+      counter--;
+      if (counter >= 0) {
+        span = document.getElementById("count");
+        span.innerHTML = counter;
+      }
+      if (counter === 0) {
+          alert('Quiz is now over!');
+          clearInterval(counter);
+      }
+    }, 1000);
+  }
+  function start()
+  {
+      document.getElementById("count").style="color:black";
+      startTimer();
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
