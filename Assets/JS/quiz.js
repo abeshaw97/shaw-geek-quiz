025a59edccfe715b9a8677389ab472322104a680
abeshaw97 <shawabra@msu.edu>
2022-03-06 22:25:30 -0500

    updated js questions

--- Assets/JS/quiz.js
+++ Assets/JS/quiz.js
@@ -1,5 +1,5 @@
 function startTimer(){
-    var counter = 50;
+    var counter = 30;
     setInterval(function() {
       counter--;
       if (counter >= 0) {
@@ -16,18 +16,33 @@ function startTimer(){
   {
       document.getElementById("count").style="color:black";
       startTimer();
+      questions()
   };
 
+  //Three questions for quiz
+  var questions = [
+{
+ question: "which file do you put all detailed info about your site on?",
+ answers, 
+ 
+    a: "CSS",
+    b: "HTML",
+    c: "Javascript",
+    answer:"B"
+},
+{
+    Question:"What command takes my data from my local cpu to the remote github repository",
+answers,
+a: "Git pull",
+b: "git fetch",
+c: "git push",
+answer: "C"
+},
+{
+Question: "What browser works best to check on our code?",
+answers,
+a: "Google Chrome",
+b: "Safari",
+c: "mozilla firefox",
+}]
 
-
-
-
-
-
-
-
-
-
-
-    
-  
